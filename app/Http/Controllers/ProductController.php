01d8c1e6877aa493694a7bdfba6c0ffb4726d2ee
Valentyn Holiuk <holiukvalentyn@gmail.com>
2023-02-04 00:17:54 +0200

    flashes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -96,6 +96,6 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
-        return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
+        return redirect()->route('products.index')->with('success', 'Product deleted successfully - '. $product->name);
     }
 }
